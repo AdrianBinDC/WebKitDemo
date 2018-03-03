e62c4663583b12b595fe39883d13d7728a7810b7
Adrian Bolinger <adrian.bolinger@me.com>
2018-03-03 10:19:12 -0500

    Initial Commit

--- WebKitDemo/ViewController.swift
+++ WebKitDemo/ViewController.swift
@@ -9,17 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  @IBOutlet weak var webView: UIWebView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    guard let url = URL(string: "https://www.youtube.com/watch?v=dQw4w9WgXcQ") else { return }
+    let request = URLRequest(url: url)
+    webView.loadRequest(request)
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
 
 }
 
